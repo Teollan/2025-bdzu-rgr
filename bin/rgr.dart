1670ea0982c280f68d14c30ff88ca6139d2d6626
Teollan <djteoprofesional@gmail.com>
2025-12-20 17:38:46 +0200

    created dart project

---
+++ bin/rgr.dart
@@ -0,0 +1,5 @@
+import 'package:rgr/rgr.dart' as rgr;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rgr.calculate()}!');
+}
---
+++ test/rgr_test.dart
@@ -0,0 +1,8 @@
+import 'package:rgr/rgr.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
